c159b08fec7f459c734badaa289c827b0d29a33c
Elian027 <elian.moreira@epn.edu.ec>
2023-12-06 13:00:42 -0500

    Pantalla de cambio de password

--- app/src/main/java/com/example/myapplication/mainActivity.java
+++ app/src/main/java/com/example/myapplication/mainActivity.java
@@ -3,17 +3,18 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 
 public class mainActivity extends AppCompatActivity {
     Button btn_cerrar;
@@ -27,16 +28,11 @@ public class mainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         btn_cerrar = findViewById(R.id.cerrar);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestore db;
         nombreTextView = findViewById(R.id.nombre);
         apellidoTextView = findViewById(R.id.apellido);
         emailTextView = findViewById(R.id.email);
         celularTextView = findViewById(R.id.celular);
-
-        nombreTextView.setText("prueba");
-        apellidoTextView.setText("prueba");
-        emailTextView.setText("prueba");
-        celularTextView.setText("prueba");
 
         btn_cerrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,30 +45,32 @@ public class mainActivity extends AppCompatActivity {
             }
 
         });
+        db = FirebaseFirestore.getInstance();
         FirebaseUser usuario = mAuth.getCurrentUser();
         if (usuario != null) {
             String usuarioId = usuario.getUid();
             DocumentReference userRef = db.collection("usuarios").document(usuarioId);
-            userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                 @Override
-                public void onComplete(Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            String nombre = document.getString("nombre_cliente");
-                            String apellido = document.getString("apellido_cliente");
-                            String email = document.getString("email_cliente");
-                            String celular = document.getString("telefono_cliente");
+                public void onSuccess(DocumentSnapshot document) {
+                    if (document.exists()) {
+                        String nombre = document.getString("nombre_cliente");
+                        String apellido = document.getString("apellido_cliente");
+                        String email = document.getString("email_cliente");
+                        String celular = document.getString("telefono_cliente");
 
-                            nombreTextView.setText("Nombre: " + nombre);
-                            apellidoTextView.setText("Apellido: " + apellido);
-                            emailTextView.setText("Email: " + email);
-                            celularTextView.setText("Celular: " + celular);
-                        }
+                        nombreTextView.setText("Nombre: " + nombre);
+                        apellidoTextView.setText("Apellido: " + apellido);
+                        emailTextView.setText("Email: " + email);
+                        celularTextView.setText("Celular: " + celular);
+                    } else {
+                        Log.d("Firestore", "El documento no existe");
                     }
                 }
             });
+
         }
 
     }
 }
+
